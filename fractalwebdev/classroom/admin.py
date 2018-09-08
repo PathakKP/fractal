aa3204870b025ea16e8bebb1ae0f5b98c0782d32
Qatil_Rangeela <35415639+Saumitra-Shukla@users.noreply.github.com>
2018-09-08 22:35:03 +0530

    Update admin.py

--- fractalwebdev/classroom/admin.py
+++ fractalwebdev/classroom/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Question,Archive
 admin.site.register(Question)
+admin.site.register(archive)
+admin.site.register(slide)
+admin.site.register(schedule)
 # Register your models here.
